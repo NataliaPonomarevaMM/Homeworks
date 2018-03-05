5f645f0393550f5bd250795dc66000eda11581be
NataliaPonomarevaMM <32074196+NataliaPonomarevaMM@users.noreply.github.com>
2018-03-05 18:14:18 +0300

    Added homework #2.4 (changed number of spaces)

--- hw2/4/4.hs
+++ hw2/4/4.hs
@@ -1,7 +1,7 @@
 help_find :: [Int] -> Int -> Int -> Int
 help_find [] _ _ = -1
 help_find (x:xs) cur n | x == n = cur
-                                                           | otherwise = help_find xs (cur + 1) n
+                       | otherwise = help_find xs (cur + 1) n
 
 find_num :: [Int] -> Int -> Int
 find_num list n = help_find list 0 n
