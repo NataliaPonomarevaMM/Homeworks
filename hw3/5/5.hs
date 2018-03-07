7a26dfecf501b125eeff4631613410bb7f30eda3
unknown <natpon12@gmail.com>
2018-03-07 13:42:40 +0300

    Added homework #3.5

---
+++ hw3/5/5.hs
@@ -0,0 +1,7 @@
+help_fib :: Int -> Int -> Int -> Int
+help_fib curr prev n | n == 0 = curr
+                                                     | n > 0  = help_fib (curr + prev) curr ( n - 1)
+		                              | n < 0  = help_fib prev (curr - prev) ( n + 1)
+
+fib :: Int -> Int
+fib n = help_fib 0 1 n
