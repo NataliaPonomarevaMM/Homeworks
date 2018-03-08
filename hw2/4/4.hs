cc7a2565ee62541711a916718086bae91b294f4f
unknown <natpon12@gmail.com>
2018-03-08 22:07:51 +0300

    Modified homework #2.4

--- hw2/4/4.hs
+++ hw2/4/4.hs
@@ -1,7 +1,3 @@
-help_find :: [Int] -> Int -> Int -> Int
-help_find [] _ _ = -1
-help_find (x:xs) cur n | x == n = cur
-                                                           | otherwise = help_find xs (cur + 1) n
-
 find_num :: [Int] -> Int -> Int
-find_num list n = help_find list 0 n
+find_num list n = if newlist == [] then (-1) else snd $ head $ newlist
+	where newlist = filter (\x -> fst x == n) (zip list [1..])
